b05f909636ccf940a59d4489f70aae315b6f877c
Stu Hood <stuhood@twitter.com>
2017-02-28 12:29:21 -0800

    Hm... any synchronization at all seems to prevent the issue. Temporarily disable, and then try to make the debug output non-blocking.

--- src/future/shared.rs
+++ src/future/shared.rs
@@ -161,7 +161,7 @@ impl<F> Future for Shared<F>
         };
         drop(state);
 
-        self.if_debug("polling!");
+        // self.if_debug("polling!");
 
         let event = task::UnparkEvent::new(unparker.clone(), 0);
         let new_state = match task::with_unpark_event(event, || original_future.poll()) {
@@ -176,6 +176,8 @@ impl<F> Future for Shared<F>
             State::Done(Ok(ref v)) => (true, Ok(SharedItem { item: v.clone() }.into())),
             State::Done(Err(ref e)) => (true, Err(SharedError { error: e.clone() }.into())),
         };
+
+        // self.if_debug(&format!("finished polling... call unpark? {:?}", call_unpark));
 
         let mut state = self.inner.state.lock().unwrap();
         match mem::replace(&mut *state, new_state) {
